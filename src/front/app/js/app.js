99f8c2a1583db9ae3dd7931bad75b4bd540e97d9
karim hamidou <khamidou@gmail.com>
2013-10-13 17:31:18 +0200

    added mail list

--- src/front/app/js/app.js
+++ src/front/app/js/app.js
@@ -4,7 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('KiteMail', ['KiteMail.filters', 'KiteMail.services', 'KiteMail.directives', 'KiteMail.controllers']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/mail', {templateUrl: 'partials/mailbox.html', controller: 'InboxListController'});
+    $routeProvider.when('/mail', {templateUrl: 'partials/mailbox.html', controller: 'InboxesListController'});
     $routeProvider.when('/login', {templateUrl: 'partials/login.html', controller: 'LoginController'});
     $routeProvider.otherwise({redirectTo: '/login'});
   }]);
--- src/front/app/js/controllers.js
+++ src/front/app/js/controllers.js
@@ -3,12 +3,36 @@
 /* Controllers */
 
 angular.module('KiteMail.controllers', []).
-  controller('InboxListController', function($scope) {
+  controller('InboxesListController', function($scope) {
         $scope.inboxes = [
             {"name": "Inbox", },
             {"name": "Trash", },
             {"name": "Spam",  },];
             
+}).controller('MailsListController', function($scope) {
+        $scope.mails = [
+            {"from": "random.spam@spam.com",
+             "to": "you",
+             "subject": "-50% promo on everything !",
+             "contents": "Everything must go !",
+             "date": new Date(),
+            },
+ 
+            {"from": "nigerian.prince@spam.com",
+             "to": "user@mail.com",
+             "subject": "I need your urgent help",
+             "contents": "Hi my dear friend. I need your help urgently in a matter of financial problems.",
+             "date": new Date(1992, 4, 24),
+            },
+            {"from": "team@kitemail",
+             "name": "Kite team",
+             "to": "user@mail.com",
+             "subject": "Welcome to Kite !",
+             "contents": "This is the basic inbox page. As you can see, it looks a lot like a famous email service.",
+             "date": new Date(),
+            }];
+    
+    
 }).controller('LoginController', function($scope) {
     
     
