a8beaf4184498631564f7eb978650e0af09e87dc
karim hamidou <khamidou@gmail.com>
2013-10-16 20:01:20 +0200

    added thread page

--- src/front/app/js/controllers.js
+++ src/front/app/js/controllers.js
@@ -34,4 +34,21 @@ angular.module('KiteMail.controllers', []).
 }]).controller('LoginController', function($scope) {
     
     
+}).controller('ThreadController', function($scope) {
+    $scope.thread = [
+        {"subject": "Hi",
+         "contents": "We haven't spoke in a long time !",
+         "name": "Michel De Test",
+         "from": "michel@example.com",
+         "to": "gerard@example.com",
+         "date": new Date(1992, 4, 24),
+        },
+        {"subject": "Hi",
+         "contents": "We haven't spoke in a long time !",
+         "from": "michel@example.com",
+         "to": "gerard@example.com",
+         "name": "Gerard Le Test",
+         "date": new Date(),
+        }
+    ]
 });
