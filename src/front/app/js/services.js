8029e7cb6197e09da8463247b269e300a453bb2f
karim hamidou <khamidou@gmail.com>
2013-10-20 20:19:32 +0200

    working on service

--- src/front/app/js/services.js
+++ src/front/app/js/services.js
@@ -6,15 +6,24 @@ angular.module('KiteMail.services', ['ngResource']).
 factory('Emails', ['$resource',
     function($resource) {
         var data = $resource('/kite/:user/mail/', {}, {
-            query: {method:'GET', params:{user: "karim"}, isArray:true}   
-        });
+            query: {
+                method:'GET',
+                params:{user: "karim"},
+                isArray:true,
+                transformResponse: function(data) {
+                    var data = angular.fromJson(data);
 
-        // convert date string to js date objects
-        for(var i = 0; i < data.length; i++) {
-            data[i]["date"] = new Date(data[i]["date"]);
-        }
+                    // convert date string to js date objects
+                    for(var i = 0; i < data.length; i++) {
+                        data[i]["date"] = new Date(data[i]["date"]);
+                    }
 
-        return data;
+                    return data;
+                },
+                
+            },
+       });
+
     }
 ]);
 
--- src/front/test/unit/services.spec.js
+++ src/front/test/unit/services.spec.js
@@ -9,7 +9,7 @@ describe('service', function() {
     it('should do an ajax request to get the list of services', inject(function($service) {
         var resourceMock = jasmine.createSpy('$resource').andReturn([{"subject": "mock", "date": "2013-04-23"}]);
         var service = $service("EmailsService", resourceMock);
-        expect(resourceMock).toHaveBeenCalled()
+        expect(resourceMock).toHaveBeenCalled();
         
     }))});
 });
