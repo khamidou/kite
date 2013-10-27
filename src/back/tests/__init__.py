40bb1e32887d8c99297f0085eb5852eb3e0bd885
karim hamidou <khamidou@gmail.com>
2013-10-27 19:01:43 +0100

    better unit tests

---
+++ src/back/tests/__init__.py
@@ -0,0 +1,1 @@
+# used to debug interactively unit tests
--- src/back/tests/test_maildir.py
+++ src/back/tests/test_maildir.py
@@ -1,6 +1,38 @@
 import kite.maildir
 import unittest
+import types
 
-class TestMaildirFunction(unittest.TestCase):
-    def test_choice(self):
-        self.assertTrue(True)
+mockdata = {"from": "gerard", "subject": "test", "contents": "empty", "to": "gerard"}
+
+class EmptyObject(object):
+    """Empty class used by mocks. Required because python doesn't let us add attributes
+    to an empty object()"""
+    pass
+
+class MockMailItem(object):
+    def __init__(self):
+        self.fp = EmptyObject()
+        self.fp.read = types.MethodType(lambda self: mockdata["contents"], self.fp)
+
+    def getheaders(self, header):
+        hd = header.lower()
+        if hd in mockdata:
+            return (mockdata[hd], "Nulltuple")
+    
+class MockMailDir(object):
+    def iteritems(self):
+        mi = MockMailItem()
+        return [("randomId", mi)]
+
+
+class TestMaildir(unittest.TestCase):
+    def test_get_mails(self):
+        md = MockMailDir()
+        parsedMaildir = kite.maildir.get_mails(md)
+        self.assertEqual(len(parsedMaildir), 1)
+
+        firstEmail = parsedMaildir[0]
+        self.assertEqual(firstEmail["id"], 0)
+
+        del firstEmail["id"] 
+        self.assertEqual(firstEmail, mockdata)
