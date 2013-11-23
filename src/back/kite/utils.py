a32bef8cf0a618f6826832c77ebc25f6d8b33896
karim hamidou <khamidou@gmail.com>
2013-11-23 20:37:48 +0100

    removed dead code

--- src/back/kite/utils.py
+++ src/back/kite/utils.py
@@ -2,10 +2,3 @@
 
 import os
 
-def stat(fd):
-    (mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime) = os.stat(file)
-    ret = {}
-    ret["atime"] = atime
-    ret["ctime"] = ctime
-    ret["mtime"] = mtime
-    return ret
