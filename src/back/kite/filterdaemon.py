9fd13b3dd1b3e4979975a458cc5a3c3beff75b18
karim hamidou <khamidou@gmail.com>
2013-11-24 10:51:20 +0100

    basic multithreaded changes detection

--- src/back/kite/filterdaemon.py
+++ src/back/kite/filterdaemon.py
@@ -2,30 +2,73 @@
 # This background process creates metadata files for threads and filters.
 #
 import sys
+import os
 import pyinotify
+import threading
+import time
+import email.parser
+
 import threads
 from jsonfile import JsonFile
 
-wm = pyinotify.WatchManager()  # Watch Manager
-mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE  # watched events
+# The architecture is pretty simple. This program is multithreaded.
+# One thread is the producer. It uses inotify to detect changes to the
+# fs. It writes in the events_queue.
+#
+# Another thread is the consumers. It processes the files.
+events_queue = []
+threads_index = {} # the email thread index - this is a dictionnary pointing to the threads filenames
 
-class EventHandler(pyinotify.ProcessEvent):
-    def process_IN_CREATE(self, event):
-        print "Creating:", event.pathname
+class WatcherThread(threading.Thread):
+    class EventHandler(pyinotify.ProcessEvent):
+        def process_IN_CREATE(self, event):
+            events_queue.append({"type": "create", "path": event.pathname})
 
-    def process_IN_DELETE(self, event):
-        print "Removing:", event.pathname
+        def process_IN_DELETE(self, event):
+            events_queue.append({"type": "delete", "path": event.pathname})
 
-def init(path):
-    handler = EventHandler()
-    notifier = pyinotify.Notifier(wm, handler)
-    wdd = wm.add_watch(path, mask, rec=True)
-    threads_index = JsonFile(os.path.join(path, "threads_index.json"))
-    if threads_index.data == None:
-        threads_index.data = {}
+    def __init__(self, path):
+        threading.Thread.__init__(self)
+        self.path = path
 
+    def run(self):
+        wm = pyinotify.WatchManager()  # Watch Manager
+        mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE  # watched events
+        handler = WatcherThread.EventHandler()
+        notifier = pyinotify.Notifier(wm, handler)
+        wdd = wm.add_watch(self.path, mask, rec=True)
+        notifier.loop()
 
-    notifier.loop()
+class ProcessorThread(threading.Thread):
+    def __init__(self, path):
+        threading.Thread.__init__(self)
+        self.path = path
+
+    def run(self):
+        threads_index = JsonFile(os.path.join(self.path, "threads_index.json"))
+        if threads_index.data == None:
+            threads_index.data = {}
+
+        while True:
+            while len(events_queue) != 0:
+                print "Found an event!"
+                event = events_queue.pop(0)
+                if event["type"] == "create":
+                    try:
+                        with open(event["path"], "r") as fd:
+                            headers = email.parser.Parser()
+                            headers.parse(fd, headersonly=True)
+                            for header in headers:
+                                print header
+                    except IOError:
+                        pass
+                    
+            time.sleep(10)
+
 
 if __name__ == "__main__":
-    init(sys.argv[1])
+    watcher_thread = WatcherThread(sys.argv[1])
+    processor_thread = ProcessorThread(sys.argv[1])
+
+    watcher_thread.start()
+    processor_thread.start()
