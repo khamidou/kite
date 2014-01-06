c8bae5bcaaad290ef0f02f895a926388b3443db7
karim hamidou <khamidou@gmail.com>
2014-01-06 20:16:33 +0100

    "refactored" fabfile

--- fabfile.py
+++ fabfile.py
@@ -7,6 +7,9 @@ from setup_config import *
 
 PACKAGES = ('rsync', 'puppet')
 
+def update_sources():
+    rsync_project("~", "../kite", exclude=[".git/", "*.swp", "*.pyc"])
+
 def provision():
     cmd = """FACTER_server_name="%s" && export FACTER_server_name && FACTER_user_home_dir=$HOME && export FACTER_user_home_dir && puppet apply $HOME/kite/manifests/server.pp --modulepath=$HOME/kite/puppet_modules""" % env.hosts[0]
     sudo(cmd)
@@ -14,10 +17,11 @@ def provision():
 def setup():
     sudo("apt-get update")
 
+    
     for package in PACKAGES:
         sudo('apt-get -y install %s' % package)
 
-    rsync_project("~", "../kite", exclude=[".git/", "*.swp", "*.pyc"])
+    update_sources()
     provision()
     local("ssh-copy-id %s@%s" % (env.user, env.hosts[0]))
     sudo("puppet apply $HOME/kite/manifests/sshd.pp --modulepath=$HOME/kite/puppet_modules")
